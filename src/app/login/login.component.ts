23338672d6ecc2d12c007ec9e9777407e5e70491
Kashinakunti Ganesh <ganesh.kashinakunti@volvo.com>
2018-09-11 20:55:17 +0530

    Login page changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,11 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    // Style to add background image only to Login page;
+    document.body.style.backgroundImage = "url('../../assets/image/volvotrucks.jpg')";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundSize = "cover";
+  }
 
   ngOnInit() {
   }
